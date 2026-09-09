b275229b1c85f9a1cbfe45bb50f48a78bea18c92
mama ceesay <mamacsay99@gmail.com>
2026-09-09 03:36:33 -1200

    feat: resized arrays - add it ems to the arrays using user keyboard

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +13,46 @@ public class Main {
          Scanner in = new Scanner(System.in);
 
 
-        String[] itemName ={"pizza","shawarma","burger",};
-        double[] itemPrices ={3.57,2.88,7.00};
-        double[] itemAvailable ={true, true, false }https://github.com/mamacsay99/Java2.git;
+        String[] itemName = {"pizza","shawarma","burger",};
+        double[] itemPrices = {3.57,2.88,7.00};
+        boolean[] itemAvailable = {true, true, false };
+
+        // add items to the Array using scanner class/object
+        do {
+            System.out.println("Are you adding?: Y or N"); // hint the user
+            String userInput = in.next();
+
+            if (userInput.equalsIgnoreCase(  "n")) break;
+            System.out.print("Enter item name: ");
+            String name = in.next();
+
+            System.out.print("Enter item price: ");
+            double price = in.nextDouble();
+
+            System.out.println("Enter item status: ");
+            boolean status = in.nextBoolean(); // true / false
+
+            System.out.println("Name \t price \t status");
+            System.out.println(name+ "\t" +price+ "\t" +status);
+
+            // add new captured item to the store
+            itemName = Arrays.copyOf(itemName, itemName.length+1);
+            // Array is resized {"Burger", "Fries", "Soda", ""}
+            itemName[itemName.length-1] = name;
+            System.out.println(Arrays.toString(itemName));
+
+        } while (true);
+
 
         for (int i=0; i < itemName.length; i++){
             //check the availabilty of an item and print
             //sold out or available
             String check = null;
-            if (itemAvailable[i]){
+
+            if (itemAvailable[i] == true){
                 check = "Available";
             } else {
-                check = "sold out"
+                check = "sold out";
             }
             System.out.println(itemName[i]+" "+itemPrices[i]+" "+check);
             }
@@ -39,4 +68,3 @@ public class Main {
         }
 
     }
-}
